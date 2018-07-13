5e945baaa04a50bd1d1cf9d97d94c853d3e710d5
Chauloroches <39598619+Chauloroches@users.noreply.github.com>
2018-07-13 22:32:21 +0300

    Add New Operations
    functions.c and functions.h updated to include subtraction, multiplication, division and modulus operations
    calc.c updated to include operation selection using a new argument 'operator'

--- calc.c
+++ calc.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 #include "functions.h"
 /*
 This is the main method
@@ -8,15 +7,26 @@ It's the entry point of our program
 */
 int main(int argc, char* argv[]){
 	int argv1, argv2;
-	printf("Calculator\n\n");
+	char operator;
 	printf("number of arguments passed: %d\n", argc);
 	printf("first argument: %s\n", argv[0]);
-	if(argc>1){
-		if(argc>=3){
-		argv1 = atoi(argv[1]);
-		argv2 = atoi(argv[2]);
-		printf("The sum of argv[1] and argv[2]: %d\n", add(argv1, argv2));
-		}
+	if(argc=4){
+		argv1 = atoi(argv[2]);
+		argv2 = atoi(argv[3]);
+		operator = (*argv[1]);
+		
+		switch (operator){
+			case '+':printf("%d + %d = %d\n", argv1, argv2, add(argv1, argv2));
+			break;
+			case '-':printf("%d - %d = %d\n", argv1, argv2, subtract(argv1, argv2));
+			break;
+			case 'x':printf("%d * %d = %d\n", argv1, argv2, multiply(argv1, argv2));
+			break;
+			case '/':printf("%d / %d = %d rem %d\n",argv1, argv2, divide(argv1, argv2),  modulus(argv1, argv2));
+			break;
+			default:
+            printf("Error!");
+			}
 	}
 	return 0;
 }
